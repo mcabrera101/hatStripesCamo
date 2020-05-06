97b9cfcd0fcb0c6f2a98a287c74a064a091024d4
Stanley Shi <stanleyshi@csu.fullerton.edu>
2020-05-06 16:49:27 -0700

    Added prompt for user input

--- simulator.cpp
+++ simulator.cpp
@@ -1,1 +1,34 @@
-//Simulator
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int MAX_MEMORY_SIZE = 30000;
+
+int memorySize, pageSize;
+
+int main(){
+  getInput();
+}
+
+void getInput(){
+  //Get user input
+  cout << "Enter memory size(0-30000): ";
+  cin >> memorySize;
+  cout << "Enter page size: ";
+  cin >> pageSize;
+
+  if(memorySize<0 || memorySize>MAX_MEMORY_SIZE )
+  {
+    cout<<"Error: Invalid input for memory size"<<endl;
+    getInput();
+  }
+  else if(pageSize<=0){
+    cout<<"Error: Invalid input for page size"<<endl;
+    getInput();
+  }
+  else if(memorySize % pageSize != 0){
+    cout<<"Error: Invalid input. Given page size does not evenly divide given memory size"<<endl;
+    getInput();
+  }
+}
